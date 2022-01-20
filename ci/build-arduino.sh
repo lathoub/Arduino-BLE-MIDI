a8cb8f3582c41fea6628d9a4ead4b7a1fc5e2f5e
lathoub <4082369+lathoub@users.noreply.github.com>
2022-01-20 05:53:13 +0100

    Update build-arduino.sh

--- ci/build-arduino.sh
+++ ci/build-arduino.sh
@@ -13,8 +13,7 @@ export PATH=$PATH:$GITHUB_WORKSPACE/bin
 curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh
 arduino-cli config init
 arduino-cli config set library.enable_unsafe_install true
-arduino-cli core update-index --additional-urls https://arduino.esp8266.com/stable/package_esp8266com_index.json
-arduino-cli core update-index --additional-urls https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json
+arduino-cli core update-index --config-file arduino-cli.yaml
 arduino-cli core update-index
 
 # Install Arduino AVR core
