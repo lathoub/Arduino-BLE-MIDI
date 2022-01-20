ada4b3ce5787ee5e73e9bb0618578ec18fadc718
lathoub <4082369+lathoub@users.noreply.github.com>
2022-01-20 07:19:50 +0100

    Update build-arduino.sh

--- ci/build-arduino.sh
+++ ci/build-arduino.sh
@@ -18,9 +18,9 @@ arduino-cli core update-index --additional-urls https://raw.githubusercontent.co
 arduino-cli core update-index -v
 
 # Install Arduino AVR core
-# arduino-cli core install arduino:avr
-# arduino-cli core install arduino:samd
-# arduino-cli core install esp8266:esp8266
+arduino-cli core install arduino:avr
+arduino-cli core install arduino:samd
+arduino-cli core install esp8266:esp8266
 arduino-cli core install esp32:esp32
 
 # List the boards
