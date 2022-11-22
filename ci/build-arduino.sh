3e83235a8b71b12403434d99347f21b713c4445b
RobertoHE <rober.herrera92@gmail.com>
2022-11-22 17:54:56 +0100

    Update build-arduino.sh
    Write directly to .yaml file

--- ci/build-arduino.sh
+++ ci/build-arduino.sh
@@ -6,15 +6,16 @@ shopt -s globstar
 # Make sure we are inside the github workspace
 cd $GITHUB_WORKSPACE
 # Create directories
-mkdir $HOME/Arduino
-mkdir $HOME/Arduino/libraries
+mkdir $HOME/Arduino -p
+mkdir $HOME/Arduino/libraries -p
 # Install Arduino IDE
 export PATH=$PATH:$GITHUB_WORKSPACE/bin
 curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh
 arduino-cli config init
 arduino-cli config set library.enable_unsafe_install true
 # arduino-cli core update-index --additional-urls https://arduino.esp8266.com/stable/package_esp8266com_index.json
-arduino-cli core update-index --additional-urls https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json
+#arduino-cli core update-index --additional-urls https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json
+sed -i 's+[]+[https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json]+g' ./.arduino15/arduino-cli.yaml
 arduino-cli core update-index
 
 # Install Arduino AVR core
@@ -24,7 +25,7 @@ arduino-cli core install arduino:samd
 arduino-cli core install esp32:esp32
 
 # List the boards
-arduino-cli board list
+arduino-cli board listall
 
 # Link Arduino library
 ln -s $GITHUB_WORKSPACE $HOME/Arduino/libraries/CI_Test_Library
