cbe1360e3b1b9495c90518efbfd5552f0f1803ba
RobertoHE <Roberto@DESKTOP-OFESHOG>
2025-05-25 14:32:07 +0200

    Fix compilation command in build script to use the correct file reference

--- ci/build-arduino.sh
+++ ci/build-arduino.sh
@@ -57,6 +57,6 @@ for f in **/*.ino ; do
      d=$(dirname $(readlink -f $f))
      echo $d
      cd $d
-     arduino-cli compile -b esp32:esp32:esp32 *.ino --clean
+     arduino-cli compile -b esp32:esp32:esp32 $f --clean
      cd $dR
 done
